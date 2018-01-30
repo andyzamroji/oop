d7428a987964bb6293e8959342a931c59ba5a44d
andyzamroji <andyzamroji@gmail.com>
2018-01-30 21:55:04 +0700

    class dan property

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+//class
+class robot{
+
+  //property
+  public $suara = "ngik ngik";
+  public $berat = "30 kg";
+}
+
+$robot1 = new robot;
+echo 'suara robotnya ' . $robot1->suara . ' dan beratnya ' . $robot1->berat;
+
+
+ ?>
