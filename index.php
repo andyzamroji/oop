fec72e39eae23e9db8bca45f355e5373f879b67c
andyzamroji <andyzamroji@gmail.com>
2018-01-30 23:26:11 +0700

    setter getter

--- index.php
+++ index.php
@@ -4,22 +4,35 @@
 class robot{
 
   //property
-  public $suara = "ngik ngik";
-  public $berat = "30 kg";
+  public $suara;
+  public $berat;
 
   //method
-  public function bersuara(){
-    echo 'suara robotnya ' . $this->suara;
+  public function set_suara($suara){
+    $this->suara = $suara;
   }
 
-  public function berat_robot(){
+  public function get_suara(){
+    return $this->suara;
+  }
+
+  public function set_berat($berat){
+    $this->berat = $berat;
+  }
+
+  public function get_berat(){
     return $this->berat;
   }
 }
 
 $robot1 = new robot;
-$robot1->bersuara();
-echo $robot1->berat_robot();
+//$robot1->bersuara();
+$robot1->set_suara("brodod");
+echo 'bunyi robot nya ' . $robot1->get_suara();
+echo '<br/>';
+
+$robot1->set_berat("40Kg");
+echo 'Berat robot nya ' . $robot1->get_berat();
 
 
  ?>
