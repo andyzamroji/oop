c4a40f81f46cb2b81c0e4d7a8aa4e9c2731c0bf9
andyzamroji <andyzamroji@gmail.com>
2018-01-31 00:17:13 +0700

    construct 2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once 'robot.php';
 
 $robot1 = new robot('Brodaaaa', 90);
+$robot1->set_berat(80);
 echo 'bunyi robot nya ' . $robot1->get_suara();
 echo '<br/>';
 echo 'Berat robot nya ' . $robot1->get_berat();
